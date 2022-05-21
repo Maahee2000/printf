9de54594f94fcbe4b93d35e7bb5fe344231bd348
maahee200 <yongzmahi99@gmail.com>
2022-05-21 10:00:25 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -34,5 +34,125 @@ int _printf(const char *format, ...)
  * Return: is an integer.
  */
 int loop_format(const char *format, va_list args)
+{
+	int i = 0, counter = 0, flag = 0, check_flag = 0, f_counter = 0;
+
+	while (i < _strlen((char *)format) && *(format + i) != '\0')
+	{
+		char charac = format[i];
+
+		if (charac == '%')
+		{
+			flag++, i++;
+			charac = format[i];
+			if (charac == '\0' && _strlen((char *)format) == 1)
+				return (-1);
+			if (charac == '\0')
+				return (counter);
+			if (charac == '%')
+				flag++;
+			else
+			{
+				f_counter = func_service(charac, args);
+				if (f_counter >= 0 && f_counter != -1)
+				{
+					i++;
+					if (charac = format[i];
+					if (charac == '%')
+						flag--;
+					counter += f_counter;
+				}
+				else if (f_counter == -1 && charac != '\n' && flag == 1)
+					counter += _putchar('%');
+			}
+		}
+		check_flag = check_percent(&flag, charac);
+		counter += check_flag;
+		if (check_flag == 0 && charac != '%' && charac != '\0')
+			counter += _putchar(charac), i++;
+		check_flag = 0;
+	}
+	return (counter);
+}
+
+/**
+ * check_percent - print a percentage
+ * @flag: is address of an int
+ * @charac: is a char
+ * Return: is 1 if % was printed, 0 otherwise.
+ */
+int check_percent(int *flag, char charac)
+{
+	int count = 0;
+
+	int tmp;
 
 
+
+	tmp = *flag;
+
+	if (tmp == 2 && charac == '%')
+	{
+		count = _putchar('%');
+		tmp = 0;
+	}
+	return (count);
+}
+
+/**
+ * func_service - takes arguments and prints it accordingly
+ * @charac: is a char or type determinant
+ * @args: is a va_list
+ * Return: is the number of characters printed
+ */
+int func_service(char charac, va_list args)
+{
+	int count = 0;
+
+	count = _switch(charac, args);
+
+	return (count);
+}
+
+/**
+ * _switch - switch character to find arg
+ * @arg: is a va_list argument
+ * @c: is a char
+ * Return: count of printed characters
+ */
+int _switch(char c, va_list arg)
+{
+	int count = 0;
+
+	switch (c)
+	{
+		case 'c':
+			count += print_character(arg);
+			break;
+		case 'd':
+		case 'i':
+			count += print_signInt(arg, 10);
+			break;
+		case's':
+			count += print_string(arg);
+			break;
+		case 'x':
+			count += print_base16_upper_lower(arg, "0123456789ABCDEF");
+			break;
+		case 'X':
+			count += print_base16_upper_lower(arg, "0123456789ABCDEF");
+			break;
+		case 'p':
+			count += print_addr(arg);
+			break;
+		case 'o':
+			count += print_unsignedInt(arg, 8);
+			break;
+		case 'u':
+			count += print_unsignedInt(arg, 10);
+			break;
+defult:
+			count = -1;
+	}
+	return (count);
+}
